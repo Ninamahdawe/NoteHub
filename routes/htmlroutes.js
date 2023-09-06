6264075c0274f80e4a3ca36d42052e782d205c4b
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-05 23:02:43 -0700

    added comments to htmlroutes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,19 +2,20 @@ const router = require("express").Router()
 const path = require("path")
 
 
-
+// Define a route to handle GET requests for the root URL ('/')
 router.get('/', (req, res) => {
     res.sendFile((path.join(__dirname, '../public/index.html')))
 
 });
-
+// Define a route to handle GET requests for the '/notes' URL
 
 router.get('/notes', (req, res) => {
+    // Send the 'notes.html' file as a response, using the path module to construct the file path
     res.sendFile((path.join(__dirname, '../public/notes.html')))
 
 });
 
 
 
-
+// Export the router for use in other parts of the application
 module.exports = router;
